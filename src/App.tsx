8acdde1e9a47f2b6e33838b7792eab248c46bf9b
Caleb <caleb@calebcurry.com>
2022-09-22 10:40:14 -0400

    initial comit with some grahpql

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
+
+export type Launch = {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    };
+};
+
+const GET_DATA = gql`
+    {
+        launchesPast(limit: 10) {
+            mission_name
+            launch_date_local
+            launch_site {
+                site_name_long
+            }
+        }
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_DATA);
+
+    useEffect(() => {
+        console.log(loading, error, data);
+    });
+    return (
+        <div className="App">
+            {data
+                ? data.launchesPast.map((launch: Launch) => {
+                      return (
+                          <p>
+                              {launch.mission_name +
+                                  ' ' +
+                                  launch.launch_date_local}
+                          </p>
+                      );
+                  })
+                : null}
+        </div>
+    );
 }
 
 export default App;
