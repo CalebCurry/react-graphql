c0295649f95fdc01f01ce9c780b7b4bfc4eeab53
Caleb <caleb@calebcurry.com>
2022-09-24 06:04:05 -0400

    add mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
-import { useEffect } from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import { useEffect, useState } from 'react';
 
 export type Customer = {
     id: number;
@@ -18,11 +18,42 @@ const GET_DATA = gql`
     }
 `;
 
+const MUTATE_DATA = gql`
+    mutation MUTATE_DATA($name: String!, $industry: String!) {
+        createCustomer(name: $name, industry: $industry) {
+            customer {
+                id
+                name
+            }
+        }
+    }
+`;
+
 function App() {
+    const [name, setName] = useState<string>('');
+    const [industry, setIndustry] = useState<string>('');
     const { loading, error, data } = useQuery(GET_DATA);
+    const [
+        createCustomer,
+        {
+            loading: createCustomerLoading,
+            error: createCustomerError,
+            data: createCustomerData,
+        },
+    ] = useMutation(MUTATE_DATA, {
+        refetchQueries: [
+            { query: GET_DATA }, // DocumentNode object parsed with gql
+        ],
+    });
 
     useEffect(() => {
         console.log(loading, error, data);
+        console.log(
+            createCustomer,
+            createCustomerLoading,
+            createCustomerError,
+            createCustomerData
+        );
     });
     return (
         <div className="App">
@@ -37,6 +68,38 @@ function App() {
                       );
                   })
                 : null}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    createCustomer({
+                        variables: { name: name, industry: industry },
+                    });
+                }}
+            >
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="industry">Industry:</label>
+                    <input
+                        id="industry"
+                        type="text"
+                        value={industry}
+                        onChange={(e) => {
+                            setIndustry(e.target.value);
+                        }}
+                    />
+                </div>
+                <button>Add Customer</button>
+            </form>
         </div>
     );
 }
